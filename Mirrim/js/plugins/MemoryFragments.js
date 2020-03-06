afc80fbfbb608dd60536b96e7dc107fa712db056
William Wong <wwong1@ualberta.ca>
2020-03-05 19:08:38 -0700

    Added Memory 3

--- Mirrim/js/plugins/MemoryFragments.js
+++ Mirrim/js/plugins/MemoryFragments.js
@@ -14,14 +14,16 @@ function calculateNumFragments(){
     else {
       if (item.note == "Area 1") {
         numArea1 += 1;
+        numTotal += 1;
       };
       if (item.note == "Area 2") {
         numArea2 += 1;
+        numTotal += 1;
       };
       if (item.note == "Area 3") {
         numArea3 += 1;
+        numTotal += 1;
       };
-      numTotal += 1;
     };
   });
 };
